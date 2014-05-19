80c44ea55e6d27a71cdca5fecb5f0c36ba631724
andrewgkim <andrewgkim@gmail.com>
2014-05-19 02:24:21 -0500

    Updated thru Part III

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,6 +3,31 @@ class PicturesController < ApplicationController
     @the_picture = Picture.find(params[:id])
     @the_source = @the_picture.source
     @the_caption = @the_picture.caption
+  end
+
+  def index
+    @list_of_pictures = Picture.all
+  end
+
+ def new
+  end
+
+ def create
+  @picture = Picture.new
+  @picture.caption = params[:caption]
+  @picture.source = params[:source]
+  @picture.save
+redirect_to("/all_pictures")
+  end
+
+ def destroy
+  @picture = Picture.find(params[:id]).destroy
+redirect_to("/all_pictures")
+  end
+
+ def destroy
 
   end
+
+
 end
