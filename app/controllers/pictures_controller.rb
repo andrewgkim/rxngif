9166e4ed88da054720f1dee3038ada36a4bb251d
andrewgkim <andrewgkim@gmail.com>
2014-05-19 01:50:43 -0500

    Part 1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,8 @@
 class PicturesController < ApplicationController
   def show
+    @the_picture = Picture.find(params[:id])
+    @the_source = @the_picture.source
+    @the_caption = @the_picture.caption
+
   end
 end
